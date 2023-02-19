d68038af35bbbb23a89bda4efe1eb094f0c243a2
AbaevB <abaev70@hotmail.com>
2023-02-19 11:29:22 +0700

    feat(markup): add form #1 start

--- main.js
+++ main.js
@@ -1,3 +1,17 @@
 (function(){
+// Создание заголовка приложения
+function createAppTitle(){
+  let AppTitle = document.createElement('h2');
+  AppTitle.innerHTML = title;
+  AppTitle.classList.add('display-2');
+  return AppTitle;
+}//end of createAppTitle
+// Создание формы
+function createTodoItemForm(){
 
+}// end of createTodoItemForm
+// Создание списка элементов.
+function createTodoList(){
+
+} // end of createTodoList
 } )();
